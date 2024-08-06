63cf7ec5990d7fa19c56254fe74203fd822edf4f
Isaac <isaacdugogomez@gmail.com>
2024-08-06 13:06:42 +0200

    add Matrix.add float,double,BigInteger,BigDecimal

--- app/src/main/java/io/francitoshi/Tinjar/Matrix.java
+++ app/src/main/java/io/francitoshi/Tinjar/Matrix.java
@@ -3,7 +3,8 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package io.francitoshi.tinjar;
-
+import java.math.BigDecimal;
+import java.math.BigInteger;
 /**
  *
  * @author isaac_dugo
@@ -51,6 +52,114 @@ public class Matrix
                 for (int k = 0; k < src[i][j].length; k++)
                 {
                     dst[j][k]+= src[i][j][k];
+                }
+            }
+        }
+    }
+    public static void add(float[][] dst, float[][]... src) throws DimensionException
+    {
+        for (int i = 0; i < src.length; i++)
+        {
+            if(dst.length!=src[i].length)
+            {
+                throw new DimensionException();
+            }
+            for (int j = 0; j < src[i].length; j++)
+            {
+                if(dst[j].length!=src[i][j].length)
+                {
+                    throw new DimensionException();
+                }
+                for (int k = 0; k < src[i][j].length; k++)
+                {
+                    dst[j][k]+= src[i][j][k];
+                }
+            }
+        }
+    }
+    public static void add(double[][] dst, double[][]... src) throws DimensionException
+    {
+        for (int i = 0; i < src.length; i++)
+        {
+            if(dst.length!=src[i].length)
+            {
+                throw new DimensionException();
+            }
+            for (int j = 0; j < src[i].length; j++)
+            {
+                if(dst[j].length!=src[i][j].length)
+                {
+                    throw new DimensionException();
+                }
+                for (int k = 0; k < src[i][j].length; k++)
+                {
+                    dst[j][k]+= src[i][j][k];
+                }
+            }
+        }
+    }
+    
+    public static BigInteger[][] create(int x, int y, BigInteger value)
+    {
+        BigInteger[][] m = new BigInteger[x][y];
+        for (int i=0; i<m.length;i++){
+            for(int j=0; j<m[i].length;j++){
+                m[i][j]=value;
+            }
+        }
+        return m;
+    }
+    public static BigDecimal[][] create(int x, int y, BigDecimal value)
+    {
+        BigDecimal[][] m = new BigDecimal[x][y];
+        for (int i=0; i<m.length;i++){
+            for(int j=0; j<m[i].length;j++){
+                m[i][j]=value;
+            }
+        }
+        return m;
+    }
+    
+    public static void add(BigInteger[][] dst, BigInteger[][]... src) throws DimensionException
+    {
+        for (int i = 0; i < src.length; i++)
+        {
+            if(dst.length!=src[i].length)
+            {
+                throw new DimensionException();
+            }
+            for (int j = 0; j < src[i].length; j++)
+            {
+                if(dst[j].length!=src[i][j].length)
+                {
+                    throw new DimensionException();
+                }
+                for (int k = 0; k < src[i][j].length; k++)
+                {
+                    
+                    dst[j][k]= dst[j][k].add(src[i][j][k]);
+                }
+            }
+        }
+    }
+    public static void add(BigDecimal[][] dst, BigDecimal[][]... src) throws DimensionException
+    {
+        for (int i = 0; i < src.length; i++)
+        {
+            if(dst.length!=src[i].length)
+            {
+                throw new DimensionException();
+            }
+            for (int j = 0; j < src[i].length; j++)
+            {
+                if(dst[j].length!=src[i][j].length)
+                {
+                    throw new DimensionException();
+                }
+                for (int k = 0; k < src[i][j].length; k++)
+                {
+                    
+                    dst[j][k]= dst[j][k].add(src[i][j][k]);
                 }
             }
         }
--- app/src/test/java/io/francitoshi/Tinjar/MatrixTest.java
+++ app/src/test/java/io/francitoshi/Tinjar/MatrixTest.java
@@ -4,12 +4,11 @@
  */
 package io.francitoshi.tinjar;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+import java.math.BigInteger;
+import java.math.BigDecimal;
+
 
 /**
  *
@@ -67,7 +66,7 @@ public class MatrixTest
         
     }
     @Test
-    public void testSomeMethod3()
+    public void testSomeMethodLong()
     {
         {
             long[][] exp = {{2}};
@@ -98,6 +97,132 @@ public class MatrixTest
             assertArrayEquals(exp,dst);
         }
     }
-    
-    
+    @Test
+    public void testSomeMethodFloat()
+    {
+        {
+            float[][] exp = {{2}};
+            float[][] dst = new float[1][1];
+            float[][] src = {{1}};
+            Matrix.add(dst,src,src);
+            assertArrayEquals(exp,dst);
+        }
+        {
+            float[][] exp = {{2,4},{6,8}};
+            float[][] dst = new float[2][2];
+            float[][] src = {{1,2},{3,4}};
+            Matrix.add(dst,src,src);
+            assertArrayEquals(exp,dst);
+        }
+        {
+            float[][] exp = {{2,4},{6,8},{10,12}};
+            float[][] dst = new float[3][2];
+            float[][] src = {{1,2},{3,4},{5,6}};
+            Matrix.add(dst,src,src);
+            assertArrayEquals(exp,dst);
+        }
+        {
+            float[][] exp = {{2,4,6},{8,10,12}};
+            float[][] dst = new float[2][3];
+            float[][] src = {{1,2,3},{4,5,6}};
+            Matrix.add(dst,src,src);
+            assertArrayEquals(exp,dst);
+        }
+    }
+    @Test
+    public void testSomeMethodDouble()
+    {
+        {
+            double[][] exp = {{2}};
+            double[][] dst = new double[1][1];
+            double[][] src = {{1}};
+            Matrix.add(dst,src,src);
+            assertArrayEquals(exp,dst);
+        }
+        {
+            double[][] exp = {{2,4},{6,8}};
+            double[][] dst = new double[2][2];
+            double[][] src = {{1,2},{3,4}};
+            Matrix.add(dst,src,src);
+            assertArrayEquals(exp,dst);
+        }
+        {
+            double[][] exp = {{2,4},{6,8},{10,12}};
+            double[][] dst = new double[3][2];
+            double[][] src = {{1,2},{3,4},{5,6}};
+            Matrix.add(dst,src,src);
+            assertArrayEquals(exp,dst);
+        }
+        {
+            double[][] exp = {{2,4,6},{8,10,12}};
+            double[][] dst = new double[2][3];
+            double[][] src = {{1,2,3},{4,5,6}};
+            Matrix.add(dst,src,src);
+            assertArrayEquals(exp,dst);
+        }
+    }
+    @Test
+    public void testSomeMethodBigInteger()
+    {
+        {
+            BigInteger[][] exp = {{BigInteger.valueOf(2)}};
+            BigInteger[][] dst = Matrix.create(1,1,BigInteger.ZERO);
+            BigInteger[][] src = {{BigInteger.valueOf(1)}};
+            Matrix.add(dst,src,src);
+            assertArrayEquals(exp,dst);
+        }
+        {
+            BigInteger[][] exp = {{BigInteger.valueOf(2),BigInteger.valueOf(4)},{BigInteger.valueOf(6),BigInteger.valueOf(8)}};
+            BigInteger[][] dst = Matrix.create(2,2,BigInteger.ZERO);
+            BigInteger[][] src = {{BigInteger.valueOf(1),BigInteger.valueOf(2)},{BigInteger.valueOf(3),BigInteger.valueOf(4)}};
+            Matrix.add(dst,src,src);
+            assertArrayEquals(exp,dst);
+        }
+        {
+            BigInteger[][] exp = {{BigInteger.valueOf(2),BigInteger.valueOf(4)},{BigInteger.valueOf(6),BigInteger.valueOf(8)},{BigInteger.valueOf(10),BigInteger.valueOf(12)}};
+            BigInteger[][] dst = Matrix.create(3,2,BigInteger.ZERO);
+            BigInteger[][] src = {{BigInteger.valueOf(1),BigInteger.valueOf(2)},{BigInteger.valueOf(3),BigInteger.valueOf(4)},{BigInteger.valueOf(5),BigInteger.valueOf(6)}};
+            Matrix.add(dst,src,src);
+            assertArrayEquals(exp,dst);
+        }
+        {
+            BigInteger[][] exp = {{BigInteger.valueOf(2),BigInteger.valueOf(4),BigInteger.valueOf(6)},{BigInteger.valueOf(8),BigInteger.valueOf(10),BigInteger.valueOf(12)}};
+            BigInteger[][] dst = Matrix.create(2,3,BigInteger.ZERO);
+            BigInteger[][] src = {{BigInteger.valueOf(1),BigInteger.valueOf(2),BigInteger.valueOf(3)},{BigInteger.valueOf(4),BigInteger.valueOf(5),BigInteger.valueOf(6)}};
+            Matrix.add(dst,src,src);
+            assertArrayEquals(exp,dst);
+        }
+    }
+    @Test
+    public void testSomeMethodBigDecimal()
+    {
+        {
+            BigDecimal[][] exp = {{BigDecimal.valueOf(2)}};
+            BigDecimal[][] dst = Matrix.create(1,1,BigDecimal.ZERO);
+            BigDecimal[][] src = {{BigDecimal.valueOf(1)}};
+            Matrix.add(dst,src,src);
+            assertArrayEquals(exp,dst);
+        }
+        {
+            BigDecimal[][] exp = {{BigDecimal.valueOf(2),BigDecimal.valueOf(4)},{BigDecimal.valueOf(6),BigDecimal.valueOf(8)}};
+            BigDecimal[][] dst = Matrix.create(2,2,BigDecimal.ZERO);
+            BigDecimal[][] src = {{BigDecimal.valueOf(1),BigDecimal.valueOf(2)},{BigDecimal.valueOf(3),BigDecimal.valueOf(4)}};
+            Matrix.add(dst,src,src);
+            assertArrayEquals(exp,dst);
+        }
+        {
+            BigDecimal[][] exp = {{BigDecimal.valueOf(2),BigDecimal.valueOf(4)},{BigDecimal.valueOf(6),BigDecimal.valueOf(8)},{BigDecimal.valueOf(10),BigDecimal.valueOf(12)}};
+            BigDecimal[][] dst = Matrix.create(3,2,BigDecimal.ZERO);
+            BigDecimal[][] src = {{BigDecimal.valueOf(1),BigDecimal.valueOf(2)},{BigDecimal.valueOf(3),BigDecimal.valueOf(4)},{BigDecimal.valueOf(5),BigDecimal.valueOf(6)}};
+            Matrix.add(dst,src,src);
+            assertArrayEquals(exp,dst);
+        }
+        {
+            BigDecimal[][] exp = {{BigDecimal.valueOf(2),BigDecimal.valueOf(4),BigDecimal.valueOf(6)},{BigDecimal.valueOf(8),BigDecimal.valueOf(10),BigDecimal.valueOf(12)}};
+            BigDecimal[][] dst = Matrix.create(2,3,BigDecimal.ZERO);
+            BigDecimal[][] src = {{BigDecimal.valueOf(1),BigDecimal.valueOf(2),BigDecimal.valueOf(3)},{BigDecimal.valueOf(4),BigDecimal.valueOf(5),BigDecimal.valueOf(6)}};
+            Matrix.add(dst,src,src);
+            assertArrayEquals(exp,dst);
+        }
+    }
 }
